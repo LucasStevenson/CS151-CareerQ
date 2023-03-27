f20a0286622503dba58de9e3f571b45d91f9a72b
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2023-03-27 02:00:28 -0700

    initial springboot setup

---
+++ careerq/src/main/java/com/example/careerq/CareerqApplication.java
@@ -0,0 +1,13 @@
+package com.example.careerq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerqApplication.class, args);
+	}
+
+}
