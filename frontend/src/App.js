e997a58ba19954395fe342a5fe217564608f97b2
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2023-05-04 05:27:25 -0700

    adding subpages to view individual events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-//import Events from "./components/Events";
-import StudentPage from "./components/StudentPage";
+import EventsPage from "./pages/EventsPage";
+import StudentPage from "./pages/StudentPage";
 import AdminPage from "./components/AdminPage";
 import CompanyPage from "./components/CompanyPage";
+import SingleEvent from "./components/SingleEvent";
 
 function App() {
   return (
@@ -18,6 +18,8 @@ function App() {
             <Route path="/student" element={<StudentPage />} />
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/company" element={<CompanyPage />} /> 
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/events/:id" element={<SingleEvent />} />
         </Routes>
     </Router>
   )
