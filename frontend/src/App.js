b5bb65c0f91c0a4d11b89ccf8063eca3c0da2113
James Kim <73400219+shallowsmith@users.noreply.github.com>
2023-05-04 08:15:45 -0700

    added profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,23 @@ import StudentPage from "./pages/StudentPage";
 import AdminPage from "./pages/AdminPage";
 import CompanyPage from "./pages/CompanyPage";
 import SingleEventPage from "./pages/SingleEventPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <Router>
-        <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/student" element={<StudentPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/company" element={<CompanyPage />} /> 
-            <Route path="/events" element={<EventsPage />} />
-            <Route path="/events/:id" element={<SingleEventPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/student" element={<StudentPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/events/:id" element={<SingleEventPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import styles from "../Module/Profile.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  //Need to change this code so that it fetches the user's information from the database
+  const user = {
+    name: "John Doe",
+    email: "test@example.com",
+    profilePic: "/profile.png",
+  };
+
+  const navigate = useNavigate();
+
+  //Need to change this code so that it deletes the jwt from the cookies
+  const handleLogOut = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className={styles.profile}>
+      <h1>Profile</h1>
+      <div className={styles.profileInfo}>
+        <img
+          src={user.profilePic}
+          alt={`${user.name}'s profile`}
+          className={styles.profilePicture}
+        />
+        <div className={styles.profileDetails}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      </div>
+      <Button variant="primary" size="sm" onClick={handleLogOut}>
+        Log Out
+      </Button>
+    </div>
+  );
+};
+
+export default Profile;
